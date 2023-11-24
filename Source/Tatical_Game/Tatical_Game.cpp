76afffdfd300af0a98e54af19c1184923a7300af
探敏 <bianchenyu.bcy@alibaba-inc.com>
2023-11-24 15:37:19 +0800

    feat: Tatical Combat 2 - Project Setup

---
+++ Source/Tatical_Game/Tatical_Game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tatical_Game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tatical_Game, "Tatical_Game" );
